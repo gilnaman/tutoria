1f0712d3093814f45cca6005b9857135292c7f03
JOSÉ GILBERTO BALAM BALAM <gilbal@hotmail.com>
2018-11-07 12:31:00 -0600

    Eliminación de íconos
    Se ocultan elementos html que no se usarán, por ahora

--- resources/views/layouts/adminlte.blade.php
+++ resources/views/layouts/adminlte.blade.php
@@ -79,11 +79,16 @@ desired effect
           <!-- Messages: style can be found in dropdown.less-->
           <li class="dropdown messages-menu">
             <!-- Menu toggle button -->
+            
+            {{--
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <i class="fa fa-envelope-o"></i>
               <span class="label label-success">4</span>
             </a>
+            --}}
+
             <ul class="dropdown-menu">
+
               <li class="header">You have 4 messages</li>
               <li>
                 <!-- inner menu: contains the messages -->
@@ -113,6 +118,7 @@ desired effect
           <!-- /.messages-menu -->
 
           <!-- Notifications Menu -->
+          {{--
           <li class="dropdown notifications-menu">
             <!-- Menu toggle button -->
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
@@ -135,14 +141,20 @@ desired effect
               <li class="footer"><a href="#">View all</a></li>
             </ul>
           </li>
+          --}}
+         
+
           <!-- Tasks Menu -->
           <li class="dropdown tasks-menu">
             <!-- Menu Toggle Button -->
+            {{--
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <i class="fa fa-flag-o"></i>
               <span class="label label-danger">9</span>
             </a>
+            --}}
             <ul class="dropdown-menu">
+              {{--
               <li class="header">You have 9 tasks</li>
               <li>
                 <!-- Inner menu: contains the tasks -->
@@ -166,6 +178,8 @@ desired effect
                   </li>
                   <!-- end task item -->
                 </ul>
+                --}}
+
               </li>
               <li class="footer">
                 <a href="#">View all tasks</a>
@@ -224,9 +238,12 @@ desired effect
             </ul>
           </li>
           <!-- Control Sidebar Toggle Button -->
+          
+          {{--
           <li>
             <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
           </li>
+          --}}
         </ul>
       </div>
     </nav>
@@ -291,12 +308,15 @@ desired effect
     <section class="content-header">
       <h1>
         @yield('titulo')
-        <small>DAP II</small>
+        <small></small>
       </h1>
+
+      {{--
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
         <li class="active">Here</li>
       </ol>
+      --}}
     </section>
 
     <!-- Main content -->
@@ -313,7 +333,7 @@ desired effect
   <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-      Anything you want
+      {{Session::get('grupo')}}
     </div>
     <!-- Default to the left -->
     <strong>Universidad Tecnológica del centro &copy; 2018 <a href="#">www.utcentro.edu.mx</a>.</strong> Todos los derechos reservados.
