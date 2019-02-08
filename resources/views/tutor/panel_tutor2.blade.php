c70c75397ea164858a3b19b4c9d392eea038c2f0
JOSÉ GILBERTO BALAM BALAM <gilbal@hotmail.com>
2019-02-08 11:01:15 -0600

    Revert "Revert "Lista para tutores""
    This reverts commit 317953614527f4fc479f9e7de6a02dab273c1b5a.

--- resources/views/tutor/panel_tutor2.blade.php
+++ resources/views/tutor/panel_tutor2.blade.php
@@ -6,7 +6,10 @@
    
 		<div class="box">
             <div class="box-header">
-              <h3 class="box-title">Listado de tutorados</h3>
+              <h3 class="box-title">Listado de tutorados  </h3> <span 
+              class="glyphicon glyphicon-print btn btn-sm btn-success" v-on:click="showLista()">
+                
+              </span>
 
             </div>
             <!-- /.box-header -->
