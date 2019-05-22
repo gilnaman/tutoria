2b407b5ec9b9ee1da415ca134965b2080bcd2eb0
JOSÉ GILBERTO BALAM BALAM <gilbal@hotmail.com>
2019-05-22 11:38:29 -0500

    Conexion de BD para acceso a boletas
    Omision de este archivo para generacion de boletas

--- config/database.php
+++ config/database.php
@@ -69,6 +69,20 @@ return [
             'engine' => null,
         ],
 
+        'boletas' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST3', 'localhost'),
+            'port' => env('DB_PORT3', '3306'),
+            'database' => env('DB_DATABASE3', 'seutc_boletas19a'),
+            'username' => env('DB_USERNAME3', 'root'),
+            'password' => env('DB_PASSWORD3', ''),
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
