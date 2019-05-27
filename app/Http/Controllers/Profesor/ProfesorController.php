f2f5890b32920bf4dbd5de65fa2d9e30f2c472f8
JOSÉ GILBERTO BALAM BALAM <gilbal@hotmail.com>
2019-05-27 12:23:39 -0500

    Descarga de listas por bloque 2019B
    Generacion de listas de asistencia por bloques 2019B

--- app/Http/Controllers/Profesor/ProfesorController.php
+++ app/Http/Controllers/Profesor/ProfesorController.php
@@ -38,6 +38,7 @@ class ProfesorController extends Controller
                     ->select('nombre')
                     ->where('ClaveAsig','=',$claveasig)
                     ->get();
+            
             foreach($asignatura as $asigna){
                 $asig=$asigna->nombre;
             }
@@ -58,17 +59,31 @@ class ProfesorController extends Controller
 
         
 
-        //$grupo,$asignatura,$unidad
-        //$grupo='TTS-4B';
-        $alumnos=Alumno::where('grupoactual','=',$clavegrupo)
-        ->where('bajadefinitiva','=',0)
-        ->orderBy('apellidop','ASC')
-        ->get();
+        // $grupo,$asignatura,$unidad
+        // $grupo='TTS-4B';
+
+        // $alumnos=Alumno::where('grupoactual','=',$clavegrupo)
+        // ->where('bajadefinitiva','=',0)
+        // ->orderBy('apellidop','ASC')
+        // ->get();
+            
         //return $alumno;
 
-if ($clavegrupo=='TTH-2A'){
+
+        // OBTENGO LISTADO DE ALUMNOS
+
+        $alumnos = DB::Select("SELECT alumnos.matricula,concat(alumnos.apellidop,' ', alumnos.apellidom,' ',alumnos.nombre) as alumno
+            FROM alumnos INNER JOIN alumnos_grupo on alumnos.matricula=alumnos_grupo.matricula
+            WHERE alumnos_grupo.periodo='2019B' and alumnos_grupo.clave_grupo='$clavegrupo'
+            ORDER BY alumnos.apellidop ASC")->get();
+
+        return $alumnos;
+
+        // FIN DE OBTENER ALUMNOS
+
+
     $pdf = new FPDF('L', 'mm', 'A4'); //Creamos un objeto de la librería horizontal o vertical, tamaño en milimetros y tipo de hoja
-for ($i=0; $i < 1; $i++)
+for ($i=0; $i < 7; $i++)
 { 
     $pdf->AddPage();
     $pdf->SetXY(10,10);
@@ -88,7 +103,7 @@ for ($i=0; $i < 1; $i++)
     $pdf->Cell(20,4,'DOCENTE: ',0,0,'C');
     $pdf->Cell(72,4,utf8_decode(Session::get('usuario')),'B',0,'C');
     $pdf->Cell(64.5,4,'UNIDAD: ',0,0,'R');
-    $pdf->Cell(20,4,$unidad,'B',0,'C');
+    $pdf->Cell(20,4,$i+1,'B',0,'C');
     $pdf->Cell(54.5,4,'CUATRIMESTRE: ',0,0,'R');
     $pdf->Cell(30,4,$cuatrimestre,'B',1,'C');
     $pdf->Cell(10.5);
@@ -148,13 +163,13 @@ for ($i=0; $i < 1; $i++)
             $fila++;
             $ix = $ix+1;
             $matricula =$alumno->matricula;
-            $nombre_alumno = "$alumno->apellidop $alumno->apellidom $alumno->nombre";
+            $nombre_alumno = "";
             $pdf->Cell(5.5);
             $pdf->SetFont('Arial','B',7);
             $pdf->Cell(5,3.9,$ix,1,0,'C');
             $pdf->SetFont('Arial','',7);
-            $pdf->Cell(20,3.9,$matricula,1,0,'C');
-            $pdf->Cell(72,3.9,utf8_decode($nombre_alumno),1,0,'L');
+            $pdf->Cell(20,3.9,$alumno->matricula,1,0,'C');
+            $pdf->Cell(72,3.9,utf8_decode($alumno->alumnno),1,0,'L');
             $pdf->Cell(6.5,3.9,'',1);
             $pdf->Cell(6.5,3.9,'',1);
             $pdf->Cell(6.5,3.9,'',1);
@@ -236,180 +251,16 @@ for ($i=0; $i < 1; $i++)
 
 
 //$pdf->Output(); 
-}
-else
-{
-    	
-$pdf = new FPDF('L', 'mm', 'A4'); //Creamos un objeto de la librería horizontal o vertical, tamaño en milimetros y tipo de hoja
-for ($i=0; $i < 1; $i++)
-{ 
-    $pdf->AddPage();
-    $pdf->SetXY(10,15);
-    $pdf-> Image(public_path().'/imagenes/logos/logo.png', 25, 13, 30);
-    $pdf->SetFont('Arial','B', 11);
-    $pdf->Cell(277,4,utf8_decode('UNIVERSIDAD TECNOLÓGICA DEL CENTRO'),0,1,'C');
-    $pdf->SetFont('Arial','B', 9);
-    $pdf->Cell(277,4,'DEPARTAMENTO DE SERVICIOS ESOLARES',0,1,'C');
-    $pdf->SetFont('Arial','B', 7);
-    $pdf->Cell(277,4,'LISTA DE ASISTENCIA',0,1,'C');
-    $pdf->Ln(10);
-    $pdf->SetFillColor(206,250,0);
-    $pdf->SetFont('Arial','B',7); 
-    $pdf->SetXY(10,26);
-    $pdf->Cell(277,4,utf8_decode($ncar),0,1,'C');
-    $pdf->Cell(10.5);
-    $pdf->Cell(20,4,'DOCENTE: ',0,0,'C');
-    $pdf->Cell(72,4,utf8_decode(Session::get('usuario')),'B',0,'C');
-    $pdf->Cell(64.5,4,'UNIDAD: ',0,0,'R');
-    $pdf->Cell(20,4,$unidad,'B',0,'C');
-    $pdf->Cell(54.5,4,'CUATRIMESTRE: ',0,0,'R');
-    $pdf->Cell(30,4,$cuatrimestre,'B',1,'C');
-    $pdf->Cell(10.5);
-    $pdf->Cell(20,4,'MATERIA:',0,0,'C');
-    $pdf->Cell(72,4,utf8_decode($asig),'B',0,'C');
-    $pdf->Cell(64.5,4,'GRUPO: ',0,0,'R');
-    $pdf->Cell(20,4,$clavegrupo,'B',0,'C');
-    $pdf->Cell(54.5,4,'PERIODO: ',0,0,'R');
-    $pdf->Cell(30,4,'ENERO-ABRIL 2019','B',1,'C');
 
-    $pdf->SetXY(10,41);
-    $pdf->SetFont('Arial','B',9); 
-    $pdf->Cell(5.5);
-    $pdf->Cell(97,6,'DATOS DEL ALUMNO',1,0,'C');
-    $pdf->Cell(169,6,'',1,1);
 
-    $pdf->SetFillColor(216,216,216);
-    $pdf->SetFont('Arial','B',7);
-    $pdf->Cell(5.5);
-    $pdf->Cell(5,4,utf8_decode('N°'),1,0,'C',1);
-    $pdf->Cell(20,4,utf8_decode('MATRÍCULA'),1,0,'C',1);
-    $pdf->Cell(72,4,'NOMBRE',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,0,'C',1);
-    $pdf->Cell(6.5,4,'',1,1,'C',1);
 
-    //$resultado = mysql_query("SELECT * FROM lista");
-
-        $ix = 0;
-        $fila=0;
-        //while($fila = mysql_fetch_array($resultado))
-        foreach($alumnos as $alumno)
-        {
-        	$fila++;
-            $ix = $ix+1;
-            $matricula =$alumno->matricula;
-            $nombre_alumno = "$alumno->apellidop $alumno->apellidom $alumno->nombre";
-            $pdf->Cell(5.5);
-            $pdf->SetFont('Arial','B',7);
-            $pdf->Cell(5,4,$ix,1,0,'C');
-            $pdf->SetFont('Arial','',7);
-            $pdf->Cell(20,4,$matricula,1,0,'C');
-            $pdf->Cell(72,4,utf8_decode($nombre_alumno),1,0,'L');
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1,1);
-        }
-        
-        while($ix < 32)
-        {
-            $ix = $ix+1;
-            $pdf->Cell(5.5);
-            $pdf->SetFont('Arial','B',7);
-            $pdf->Cell(5,4,$ix,1,0,'C');
-            $pdf->Cell(20,4,'',1,0,'C');
-            $pdf->Cell(72,4,'',1,0,'L');
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1);
-            $pdf->Cell(6.5,4,'',1,1);
-        }
-
-            $pdf->SetXY(10,183);
-            $pdf->Cell(70.5,4);
-            $pdf->SetFont('Arial','B',7);
-            $pdf->Cell(30,4);
-            $pdf->Cell(76,4,'FIRMA DEL DOCENTE','T',0,'C');
-            $pdf->Cell(30,4);
-            $pdf->SetFont('Arial','',7);
-            $pdf->Cell(70.5,4,'F-UTC-SES-05/V04',0,1,'L');
-    
-}
-
-			$headers=['Content-Type'=>'aplication/pdf'];
-
-            return Response::make($pdf->Output(),200,$headers);
 }
 
 //$pdf->Output(); 
 
-    }
+    
+
+
+    
 }
 
